d41d4320eae653b2b36d7ec3cac03bee36199dcf
Woong <wjsdnd04@naver.com>
2021-09-23 11:16:46 +0900

    9/23 6ban class commit 4

--- WebApplication_project_1/settings/deploy.py
+++ WebApplication_project_1/settings/deploy.py
@@ -1,33 +1,24 @@
 from .base import *
 
-def read_secret(secret_name):
+env_list = dict()
 
-    file = open('/run/secrets/' + secret_name)
-    secret = file.read()
-    secret = secret.rstrip().lstrip()
-    file.close()
+local_env = open(os.path.join(BASE_DIR, '.env'))
 
-    return secret
-
-# env_list = dict()
-#
-# local_env = open(os.path.join(BASE_DIR, '.env'))
-#
-# while True:
-#     line = local_env.readline()
-#     if not line:
-#         break
-#     line = line.replace('\n','')
-#     start = line.find('=')
-#     key = line[:start]
-#     value = line[start+1:]
-#     env_list[key] = value
+while True:
+    line = local_env.readline()
+    if not line:
+        break
+    line = line.replace('\n','')
+    start = line.find('=')
+    key = line[:start]
+    value = line[start+1:]
+    env_list[key] = value
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret['DJANGO_SECRET_KEY']
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -43,7 +34,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'PASSWORD': 'password1234',
         'HOST': 'mariadb',
         'PORT': '3306',
     }
